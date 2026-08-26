e6c4cc2897bcfafb7abca25e881fe6a0f8b8d595
ThietKeHDT <lietthuan54@gmail.com>
2026-08-26 16:24:32 +0700

    Add VerifyByPhone and VerifyByEmail folder

--- src/features/auth/screens/index.ts
+++ src/features/auth/screens/index.ts
@@ -5,3 +5,5 @@ export { default as ForgotPasswordScreen } from "./forgotPassword";
 export { default as VerifyResetCodeScreen } from "./verifyResetCode";
 export { default as ResetPasswordScreen } from "./resetPassword";
 export { default as WelcomeScreen } from "./welcome";
+export { default as VerifyByEmailScreen } from "./verifyByEmail";
+export { default as VerifyByPhoneScreen } from "./verifyByPhone";
---
+++ src/features/auth/screens/verifyByEmail/index.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+const VerifyByEmail = () => {
+  return (
+    <View></View>
+  )
+}
+
+export default VerifyByEmail;
